1fa51460116550c34de0c01ac893de16af9a792e
mail@sebastian-knopf.de <mail@sebastian-knopf.de>
2025-12-25 08:59:28 +0100

    fixed some smaller issues in MqttPublisher

--- service/waltrac/mqtt.py
+++ service/waltrac/mqtt.py
@@ -1,5 +1,7 @@
 import asyncio
 import logging
+import random
+import string
 
 from paho.mqtt import client
 
@@ -22,6 +24,9 @@ class MqttPublisher:
             self._task = asyncio.create_task(self._run())
 
     async def stop(self):
+        self._mqtt.loop_stop()
+        self._mqtt.disconnect()
+
         if self._task is not None and not self._task.done():
             self._task.cancel()
 
@@ -33,7 +38,10 @@ class MqttPublisher:
         self._task = None
 
     async def _run(self):
-        self._mqtt = client.Client(client.CallbackAPIVersion.VERSION2, protocol=client.MQTTv5, client_id='waltrac-gateway')
+        client_id_seed: str = ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(8))
+        client_id: str = f"waltrac-gateway-{client_id_seed}"
+
+        self._mqtt: client.Client = client.Client(client.CallbackAPIVersion.VERSION2, protocol=client.MQTTv5, client_id=client_id)
 
         if self._username is not None and self._password is not None:
             self._mqtt.username_pw_set(username=self._username, password=self._password)
@@ -41,13 +49,14 @@ class MqttPublisher:
         self._mqtt.connect(self._host, self._port)
         self._mqtt.loop_start()
 
+        logging.info(f"Connected to MQTT broker at {self._host}:{self._port}.")
         self._connected.set()
 
     async def publish(self, topic: str, payload: bytes):
         await self._connected.wait()
         
         try:
-            await self._mqtt.publish(f"{self._toplevel}/{topic}", payload)
+            self._mqtt.publish(f"{self._toplevel}/{topic}", payload)
         except Exception:
             self._connected.clear()
             
